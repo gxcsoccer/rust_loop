2343a01649d47cb915685fc6c12beb4cfdf6e30f
xiaochen.gao <xiaochen.gao@nio.com>
2024-05-19 16:28:52 +0800

    refactor: 抽象公共方法

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+fn fib_next(a: &mut i32, b: &mut i32) {
+    let c = *a + *b;
+    *a = *b;
+    *b = c;
+}
+
 fn fib_loop(n: u8) {
     let mut a = 1;
     let mut b = 1;
     let mut i = 2u8;
 
     loop {
-        let c = a + b;
-        a = b;
-        b = c;
+        fib_next(&mut a, &mut b);
         i += 1;
 
         println!("next val is {}", b);
@@ -21,9 +25,7 @@ fn fib_while(n: u8) {
     let (mut a, mut b, mut i) = (1, 1, 2u8);
 
     while i < n {
-        let c = a + b;
-        a = b;
-        b = c;
+        fib_next(&mut a, &mut b);
         i += 1;
 
         println!("next val is {}", b);
@@ -34,9 +36,7 @@ fn fib_for(n: u8) {
     let (mut a, mut b) = (1, 1);
 
     for _i in 2..n {
-        let c = a + b;
-        a = b;
-        b = c;
+        fib_next(&mut a, &mut b);
         println!("next val is {}", b);
     }
 }
